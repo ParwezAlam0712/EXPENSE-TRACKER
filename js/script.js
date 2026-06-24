984ce4fb2f34a6c24ba5d2c71d48c89d9dea2194
Parwez Alam <mdp311090@gmail.com>
2026-06-24 21:07:24 +0530

    Add transaction type selection for income and expense

--- js/script.js
+++ js/script.js
@@ -29,20 +29,34 @@ console.log("ExpenseFlow Pro Started");
 console.log(transactions);
 
 function init() {
-    console.log("Application Initialized");
+
+    renderTransactions();
+
+    console.log(
+        "Application Initialized"
+    );
+
 }
 
-init();
 
 function addTransaction(e) {
+
     e.preventDefault();
 
     const text = textInput.value.trim();
-    const amount = Number(amountInput.value);
+
+    const type = document.getElementById("type").value;
+
+    let amount = Number(amountInput.value);
+
     const category = categoryInput.value;
 
+    if (type === "expense") {
+        amount = -amount;
+    }
+
     if (text === "" || amount === 0) {
-        alert("Please enter valid data");
+        alert("Please enter valid details");
         return;
     }
 
@@ -50,14 +64,100 @@ function addTransaction(e) {
         id: Date.now(),
         text,
         amount,
-        category
+        category,
+        type
     };
 
     transactions.push(transaction);
+
+    renderTransactions();
 
     console.log(transactions);
 
     form.reset();
 }
 
+init();
+
+
+
 form.addEventListener("submit", addTransaction);
+
+function renderTransactions() {
+
+    transactionsList.innerHTML = "";
+
+    transactions.forEach((transaction) => {
+
+        const li = document.createElement("li");
+
+        li.classList.add(
+            transaction.amount > 0
+                ? "income"
+                : "expense"
+        );
+
+        li.innerHTML = `
+    <div>
+        <strong>${transaction.text}</strong>
+        <br>
+        <small>${transaction.category}</small>
+    </div>
+
+     <div>
+        ₹${Math.abs(transaction.amount)}
+
+         <button
+            class="delete-btn"
+            onclick="deleteTransaction(${transaction.id})">
+            ❌
+         </button>
+     </div>
+    `;
+
+        transactionsList.appendChild(li);
+
+    });
+
+
+    updateSummary();
+
+}
+
+function deleteTransaction(id) {
+
+    transactions =
+        transactions.filter(
+            transaction =>
+                transaction.id !== id
+        );
+
+    renderTransactions();
+}
+
+function updateSummary() {
+
+    const amounts = transactions.map(
+        transaction => transaction.amount
+    );
+
+    const total = amounts
+        .reduce((acc, item) => acc + item, 0);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => acc + item, 0);
+
+    const expense = amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => acc + item, 0);
+
+    balanceEl.textContent =
+        `₹${total.toFixed(2)}`;
+
+    incomeEl.textContent =
+        `₹${income.toFixed(2)}`;
+
+    expenseEl.textContent =
+        `₹${Math.abs(expense).toFixed(2)}`;
+}
